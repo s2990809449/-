6e90a9402c028a4267dc2e800dba60b3f344f0c2
Tom Wang <s22659211@yahoo.com.tw>
2022-05-22 02:43:19 +0800

    3rd commit_更進階

--- 密碼重設程式.py
+++ 密碼重設程式.py
@@ -1,16 +1,18 @@
 # 密碼重設程式
 password = 'a123456s'
 i = 3
-while True:
+while i > 0:
+	i = i - 1
 	pws = input('請輸入密碼: ')
 	if pws == password:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
-		print('密碼錯誤 你還有', i, '次機會')
-	if i == 0:
-		break
+		if i > 0:
+			print('密碼錯誤 你還有', i, '次機會')
+		else:
+			print('登入失敗')
+
 		
 
 
